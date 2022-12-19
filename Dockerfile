a0b1c9bbc1dd09fc11bc2329cb3029ec93af19c6
Amol Deshmukh <a.deshmukh@northeastern.com>
2022-12-19 12:19:58 -0500

    2.0.0 snapshot - 2.1.0 throws numeric overflow error

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM quay.io/strimzi/kafka:0.32.0-kafka-3.2.0
 USER root:root
 RUN mkdir -p /opt/kafka/plugins/debezium
 COPY ./debezium-connector-oracle/debezium-connector-oracle/ /opt/kafka/plugins/debezium/
-COPY ./target/debezium-kafka-connector-2.1.0-SNAPSHOT-package/share/java/debezium-kafka-connector/ /opt/kafka/plugins/debezium/
+COPY ./target/debezium-kafka-connector-2.0.0-SNAPSHOT-package/share/java/debezium-kafka-connector/ /opt/kafka/plugins/debezium/
 COPY ./oracle-instantclient/instantclient_21_8/ojdbc8.jar /opt/kafka/libs/ojdbc8.jar
 USER 1001
--- publish.docker.sh
+++ publish.docker.sh
@@ -40,20 +40,20 @@ mvn install:install-file \
 # Go where https://github.com/amolde/debezium is cloned
 cd ../debezium
 
-git checkout 2.1.0-SNAPSHOT
+git checkout 2.0
 
 mvn clean install -pl debezium-connector-oracle -am -Passembly         
 
-ls -lrt debezium-connector-oracle/target/debezium-connector-oracle-2.1.0-SNAPSHOT-plugin.tar.gz  
+ls -lrt debezium-connector-oracle/target/debezium-connector-oracle-2.0.0-SNAPSHOT-plugin.tar.gz  
 
-tar tvf  debezium-connector-oracle/target/debezium-connector-oracle-2.1.0-SNAPSHOT-plugin.tar.gz  
+tar tvf  debezium-connector-oracle/target/debezium-connector-oracle-2.0.0-SNAPSHOT-plugin.tar.gz  
 
 # =========================================================
 
 # Change back to this project
 cd -
 
-docker_tag=2.1.0-SNAPSHOT
+docker_tag=2.0.0-SNAPSHOT
 
 if [[ ${docker_tag} == "" ]]
 then
@@ -62,12 +62,12 @@ then
 fi
 
 # curl -L 'https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=io.debezium&a=debezium-connector-oracle&v=LATEST&c=plugin&e=tar.gz' --output debezium-connector-oracle.tar.gz
-# https://repo1.maven.org/maven2/io/debezium/debezium-connector-oracle/2.1.0.Alpha2/debezium-connector-oracle-2.1.0.Alpha2.jar
+# https://repo1.maven.org/maven2/io/debezium/debezium-connector-oracle/2.0.0.Alpha2/debezium-connector-oracle-2.0.0.Alpha2.jar
 
 connectorplugindir=debezium-connector-oracle
 
 mkdir "${connectorplugindir}"
-cp ../debezium/debezium-connector-oracle/target/debezium-connector-oracle-2.1.0-SNAPSHOT-plugin.tar.gz ./${connectorplugindir}/debezium-connector-oracle.tar.gz
+cp ../debezium/debezium-connector-oracle/target/debezium-connector-oracle-2.0.0-SNAPSHOT-plugin.tar.gz ./${connectorplugindir}/debezium-connector-oracle.tar.gz
 
 cd "${connectorplugindir}"
 tar xvf debezium-connector-oracle.tar.gz
@@ -77,7 +77,7 @@ ls -lrt "${instantclientdir}/instantclient_21_8/ojdbc8.jar"
 
 mvn clean install
 
-ls -lrt ./target/debezium-kafka-connector-2.1.0-SNAPSHOT-package/share/java/
+ls -lrt ./target/debezium-kafka-connector-2.0.0-SNAPSHOT-package/share/java/
 
 docker build -t amolde/debezium-kafka-connect:${docker_tag} .
 
